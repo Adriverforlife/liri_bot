2ee6dfd7a73389c52dc0a2858d16899821bec106
Michael Eguina <michael@corner-exit.com>
2019-07-12 22:27:57 -0700

    spotify api now works. need to remember the require line for it

--- liri.js
+++ liri.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
+var Spotify = require('node-spotify-api');
+var axios = require("axios");
 var keys = require("./keys.js");
-//var spotify = new Spotify(keys.spotify);
-var artist = process.argv[2]
+var spotify = new Spotify(keys.spotify);
+
+var moment = require('moment');
+var search = process.argv[3]
+var action = process.argv[2]
 
 
 // Write the following commands
@@ -12,6 +17,53 @@ var artist = process.argv[2]
 // * `movie-this`
 
 // * `do-what-it-says`
+if (action == "concert-this") {
+    var queryUrl = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp"
+    axios.get(queryUrl).then(
+        function (response) {
+            console.log(search + "is playing in the following places:")
+            console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            // * Name of the venue
+            for (var i = 0; i < response.data.length; i++) {
+                console.log(response.data[i].venue.name)
+                // * Venue location
+                console.log(response.data[i].venue.city + "," + response.data[i].venue.country)
+                // * Date of the Event (use moment to format this as "MM/DD/YYYY")
+                var dateTime = moment(response.data[i].datetime).format("MM/DD/YYYY")
+                console.log(dateTime)
+                console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            }
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
 
-var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-console.log(queryUrl)
+}
+if ("spotify-this-song") {
+
+    spotify
+        .search({ type: 'track', query: 'All the Small Things' })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
