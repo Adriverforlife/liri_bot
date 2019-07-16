4c8b9e2dc0d5f24f21c5da450516b606d705535f
Michael Eguina <michael@corner-exit.com>
2019-07-15 22:07:17 -0700

    finished movie and fs

--- liriTest.js
+++ liriTest.js
@@ -3,8 +3,6 @@ var Spotify = require('node-spotify-api');
 var axios = require("axios");
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
-var fs = require("fs");
-
 
 var moment = require('moment');
 var search = process.argv[3]
@@ -19,7 +17,6 @@ var action = process.argv[2]
 // * `movie-this`
 
 // * `do-what-it-says`
-liriBot()
 function liriBot(){
 if (action == "concert-this") {
     var queryUrl = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp"
@@ -60,7 +57,7 @@ if (action == "concert-this") {
         });
 
 }
-if (action =="spotify-this-song") {
+if ("spotify-this-song") {
     if(search==undefined){
         search="The Sign"
     }
@@ -93,74 +90,62 @@ if (action =="spotify-this-song") {
             console.log(err);
         });
 }
-if(action=="movie-this"){
-    if(search==undefined){
-    search="Mr. Nobody"
-    }
-    var queryUrl = "http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy";
-    
-    
-    axios.get(queryUrl).then(
-      function(response) {
-        // * Title of the movie.
-        console.log("Title: "+response.data.Title)
-        // // * Year the movie came out.
-        console.log("Release Year: " + response.data.Year);
-        // // * IMDB Rating of the movie.
-        console.log("IMDB Rating: "+response.data.imdbRating)
-        // // * Rotten Tomatoes Rating of the movie.
-        for(var i=0;i<response.data.Ratings.length;i++){
-        if(response.data.Ratings[i].Source=="Rotten Tomatoes"){
-        console.log("Rotten Tomatoes Score: "+response.data.Ratings[i].Value)}}
-        // // * Country where the movie was produced.
-        console.log("Produced in "+response.data.Country)
-        // // * Language of the movie.
-        console.log("Language: "+response.data.Language)
-        // // * Plot of the movie.
-        console.log("Plot: "+response.data.Plot)
-        // // * Actors in the movie.
-        console.log("Cast: "+response.data.Actors)
-        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+if("movie-this"){
+if(search==undefined){
+search="Mr. Nobody"
+}
+var queryUrl = "http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy";
 
-      })
-      .catch(function(error) {
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.log("---------------Data---------------");
-          console.log(error.response.data);
-          console.log("---------------Status---------------");
-          console.log(error.response.status);
-          console.log("---------------Status---------------");
-          console.log(error.response.headers);
-        } else if (error.request) {
-          // The request was made but no response was received
-          // `error.request` is an object that comes back with details pertaining to the error that occurred.
-          console.log(error.request);
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.log("Error", error.message);
-        }
-        console.log(error.config);
-      });
-    }
-    if(action=="do-what-it-says"){
-        fs.readFile("random.txt", "utf8", function(error, data) {
 
-            // If the code experiences any errors it will log the error to the console.
-            if (error) {
-              return console.log(error);
-            }
-          
-            // We will then print the contents of data
-          
-            // Then split it by commas (to make it more readable)
-            var dataArr = data.split(",");
-            action=dataArr[0]
-            search=dataArr[1]
-            liriBot()
-            // We will then re-display the content as an array for later use.
-          
-          });
-          
-    }}
+
+axios.get(queryUrl).then(
+  function(response) {
+    // * Title of the movie.
+    console.log("Title: "+response.data.title)
+    // * Year the movie came out.
+    console.log("Release Year: " + response.data.Year);
+    // * IMDB Rating of the movie.
+    console.log("IMDB Rating: "+response.data.rating)
+    // * Rotten Tomatoes Rating of the movie.
+    console.log("Rotten Tomatoes Score: "+response.data.rating)
+    // * Country where the movie was produced.
+    console.log("Produced in "+response.data.produced.country)
+    // * Language of the movie.
+    console.log("Language: "+response.data.language)
+    // * Plot of the movie.
+    console.log("Plot: "+response.data.summary)
+    // * Actors in the movie.
+    console.log("Cast: "+response.data.cast)
+  })
+  .catch(function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  });
+
+
+}
+if("do-what-it-says"){
+    var fs = require("fs")
+    var random = 'spotify-this-song,"I Want it That Way"';
+    var res = random.split(",");
+    action = res[0]
+    search = res[1]
+    liriBot()
+}
+}
